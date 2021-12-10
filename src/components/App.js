bd625862ad56c5418334637fa36e2331e071ccd3
Madeleine Whitley <maddy.whitley@gmail.com>
2021-12-09 19:43:32 -0700

    app js file

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import ItemList from './components/item/ItemList';
+
+class App extends Component {
+  state = { item: [
+    { id: 1, title: 'Learn Ruby',  desc: 'learn ruby langauge', complete: true, },
+    { id: 2, title: 'Learn JS',  desc: 'learn js langauge', complete: true, },
+    { id: 3, title: 'Learn React',  desc: 'learn React technology', complete: false, },
+  ] }
+
+  render() {
+    const { item } = this.state
+    return (
+      <>
+        <h1>Grocery List</h1>
+        <ItemList item={item} listName="Coding" />
+      </>
+    )
+  }
+}
+
+export default App;
